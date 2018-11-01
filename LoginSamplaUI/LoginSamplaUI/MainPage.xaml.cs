3657ba7627af2b07b0f2453707616d88a2f503fb
HungTran <hungdng.92@gmail.com>
2018-11-01 17:16:31 +0700

    first commit

---
+++ LoginSamplaUI/LoginSamplaUI/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginSamplaUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SignUpPage());
+        }
+    }
+}
